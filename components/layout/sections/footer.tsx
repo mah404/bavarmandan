95e9965c97d362d97f507d78ff6517a4985ec6ec
mahtabvariyani <mahtabvariyani@gmail.com>
2025-06-11 00:53:30 +0200

    Added Color

--- components/layout/sections/footer.tsx
+++ components/layout/sections/footer.tsx
@@ -5,7 +5,7 @@ export const FooterSection = () => {
   return (
     <footer id="contact" className="container py-24 sm:py-32">
       <div className="p-10 bg-card border border-secondary rounded-2xl">
-<div className="flex flex-col xl:flex-row xl:justify-center items-center gap-12 text-center">
+        <div className="flex flex-col xl:flex-row xl:justify-center items-center gap-12 text-center">
           <div className="col-span-full xl:col-span-2 flex justify-center">
             <Link href="#" className="flex flex-col items-center font-bold">
               <img
@@ -24,16 +24,17 @@ export const FooterSection = () => {
             <Link href="#" className="opacity-60 hover:opacity-100">
               کلاب‌هاوس
             </Link>
+                <Link href="#" className="opacity-60 hover:opacity-100">
+              یوتیوب
+            </Link>
+                <Link href="#" className="opacity-60 hover:opacity-100">
+              اینستاگرام
+            </Link>
           </div>
 
           <div className="flex flex-col items-center gap-2">
-            <h3 className="font-bold text-lg">ارتباط با ما</h3>
-            <Link href="#" className="opacity-60 hover:opacity-100">
-              راه ارتباطی
-            </Link>
-            <Link href="#" className="opacity-60 hover:opacity-100">
-              نظرات شما
-            </Link>
+            <h3 className="font-bold text-lg">  ارتباط مستقیم با ما</h3>
+         
           </div>
         </div>
 
