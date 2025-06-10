fdd1622fbd8081778dbce4653d045eaabf1cbcbd
mahtabvariyani <mahtabvariyani@gmail.com>
2025-06-11 00:35:53 +0200

    Added Color

--- components/layout/sections/community.tsx
+++ components/layout/sections/community.tsx
@@ -32,7 +32,7 @@ export const CommunitySection = () => {
                 </div>
               </CardTitle>
             </CardHeader>
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground leading-relaxed">
+            <CardContent className="lg:w-[80%] font-semibold text-xl text-muted-foreground leading-relaxed">
               با توجه به اهداف شکل‌گیری جلسات «مجمع باورمندان» در کلاب‌هاوس، که
               با نیت هم‌افزایی فکری و تبادل آرا در راستای ایجاد فضایی سالم برای
               گفت‌وگو، در سایه عقل، منطق و ادب و همچنین تقویت مودت و آرامش در
@@ -40,52 +40,52 @@ export const CommunitySection = () => {
               همراهان گرامی درخواست می‌شود در جهت حفظ کیفیت جلسات، موارد زیر را
               رعایت فرمایند:
             </CardContent>
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground ">
+            <CardContent className="lg:w-[80%] text-xl text-muted-foreground  font-thin">
               🔸 رعایت آرامش، ادب، اخلاق و حفظ کرامت انسانی در رفتار، گفتار و
               نوشتار. نوشتار.
             </CardContent>
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
+            <CardContent className="lg:w-[80%] text-xl text-muted-foreground  font-thin">
               🔸 پرهیز از هرگونه توهین، تمسخر یا بی‌احترامی به عقاید، باورها و
               دیدگاه‌های دیگران و تمرکز صرف بر نقد تفکرات، نه اشخاص.
             </CardContent>
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
+            <CardContent className="lg:w-[80%] text-xl text-muted-foreground  font-thin">
               🔸 اجتناب از طرح مباحث سیاسی یا مسائل اختلاف‌برانگیز اجتماعی خارج
               از موضوع جلسه.{" "}
             </CardContent>
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
+            <CardContent className="lg:w-[80%] text-xl text-muted-foreground  font-thin">
               🔸 خودداری از پرداختن به موضوعات متفرقه یا خارج از محتوای اصلی
               ارائه‌شده در جلسه.
             </CardContent>
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
+            <CardContent className="lg:w-[80%] text-xl text-muted-foreground  font-thin">
               🔸 پرهیز از مطرح‌کردن اختلافات، منازعات یا مسائل شخصی در جلسه.
             </CardContent>
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
+            <CardContent className="lg:w-[80%] text-xl text-muted-foreground  font-thin">
               🔸 احترام به تمایل یا عدم تمایل دیگران به شرکت در گفت‌وگو؛ اصرار
               به صحبت کردن با مخاطبی که تمایلی ندارد، پذیرفته نیست.
             </CardContent>
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
+            <CardContent className="lg:w-[80%] text-xl text-muted-foreground  font-thin">
               🔸 استفاده از عکس پروفایل محترمانه و متناسب؛ عکس‌هایی با محتوای
               توهین‌آمیز نسبت به مقدسات، اشخاص، باورهای اعتقادی یا گروه‌های
               سیاسی ممنوع است.
             </CardContent>
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
+            <CardContent className="lg:w-[80%] text-xl text-muted-foreground  font-thin">
               🔸 زمان، نوبت و مدت گفت‌وگوی هر یک از شرکت‌کنندگان بر اساس تشخیص و
               صلاحدید مدیران جلسه تعیین می‌شود. لطفاً از مداخله در گفت‌وگوی
               دیگران خارج از زمان مشخص‌شده پرهیز نمایید.
             </CardContent>
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
+<CardContent className="lg:w-[80%] text-xl text-muted-foreground font-semibold ">
               بدیهی است در صورت عدم رعایت موارد فوق در جلسه، استیج یا بخش چت،
               مدیران جلسات بنا به صلاحدید خود می‌توانند از ادامه حضور افراد در
               همان جلسه یا جلسات آینده جلوگیری نمایند.
             </CardContent>
 
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
+            <CardContent className="lg:w-[80%] text-xl text-muted-foreground font-semibold">
               همچنین «مجمع باورمندان کلاب‌هاوس» بنابر نیاز و مصالح مجموعه، حق
               استفاده از محتوای صوتی، گفتاری و نوشتاری ارائه‌شده در جلسات را
               برای خود محفوظ می‌داند.
             </CardContent>
 
-            <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
+            <CardContent className="lg:w-[80%] text-xl text-muted-foreground font-semibold">
               از تمامی دوستان عزیز درخواست می‌شود به‌منظور برگزاری بهتر جلسات و
               رفع ابهامات احتمالی، پرسش‌ها، انتقادات و پیشنهادات خود را از طریق
               پیام‌رسان تلگرام (آدرس مندرج) به اطلاع مدیران برسانند. پیشاپیش از
