ee046aa46b8ffba5ad15b25afbf6c8e42ab1decd
mahtabvariyani <mahtabvariyani@gmail.com>
2025-06-11 22:45:13 +0200

    Deploy

---
+++ index.ts
@@ -0,0 +1,137 @@
+
+
+
+
+
+  // ✅ Audio files list (only title + url)
+  const audioFiles = [
+    {
+      title: "تاملاتی در من ",
+      url: "https://www.dropbox.com/scl/fi/yn7lczizzy1taskripz60/1.mp3?rlkey=kvcoqgwwxrii8m3x6ug4svfym&st=687lourl&dl=1",
+    },
+    {
+      title: "تکبر جلسه 1",
+      url: "https://www.dropbox.com/scl/fi/zqea6e5d8vbgjz6ecuc2l/2.mp3?rlkey=91xd88htnx7srj739w6mbeifx&st=4xsk09we&dl=1",
+    },
+    {
+      title: "تکبر جلسه 2",
+      url: "https://www.dropbox.com/scl/fi/7etmwkuogzmfvu7ft04xy/3.mp3?rlkey=os2ysd1ldgh2iatqms0y9y6zf&st=8ul2eapt&dl=1",
+    },
+    {
+      title: "تکبر جلسه 3",
+      url: "https://www.dropbox.com/scl/fi/jt6lctlt58mjt3jfyjv01/4.mp3?rlkey=h0ydwofk1wvr9ooqc9rowwp8u&st=dwgfd14d&dl=1",
+    },
+    {
+      title: "تکبر جلسه 4",
+      url: "https://www.dropbox.com/scl/fi/1zr6e5vxsbittcszg9usd/5.mp3?rlkey=uw6qo2khku1ogde5mz631nmdm&st=anc8arrc&dl=1",
+    },
+    {
+      title: "تکبر جلسه 5",
+      url: "https://www.dropbox.com/scl/fi/bzzbu5oehjr25kav1nv70/6.mp3?rlkey=6att9368qo7bgcwfb7xnoubrm&st=8ho804ip&dl=1",
+    },
+
+    {
+      title: "حب دنیا جلسه 1 ",
+      url: "https://www.dropbox.com/scl/fi/f09wvxxo5sy7ifu6qzkpg/7.mp3?rlkey=5vnt1nexzl6pl03qn9h1z0l16&st=1l43h5w9&dl=1",
+    },
+
+    {
+      title: "حب دنیا جلسه 2 ",
+      url: "https://www.dropbox.com/scl/fi/l6vdqt0515jcq6d9swhgh/8.mp3?rlkey=406plqjht70ban0z9dmmpkbpu&st=jfg5vfzj&dl=1",
+    },
+
+    {
+      title: "حب دنیا جلسه 3",
+      url: "https://www.dropbox.com/scl/fi/79xiee3x4d00j42wzus69/9.mp3?rlkey=p4i1rfj2jimwr24w6fqg2rkus&st=dl56r9yf&dl=1",
+    },
+
+    {
+      title: "حب دنیا جلسه 4",
+      url: "https://www.dropbox.com/scl/fi/2864z3i4kuv0asru4f7zm/10.mp3?rlkey=jven8z7axkbbdncv2l1uh28v2&st=kd806nvi&dl=1",
+    },
+
+    {
+      title: "بخل جلسه 1 ",
+      url: "https://www.dropbox.com/scl/fi/82ohswqxjc9p7cj9na34r/11.mp3?rlkey=rsjqugxoprpetwrx0154xl6h8&st=pb41q7cf&dl=1",
+    },
+
+    {
+      title: "بخل جلسه 2",
+      url: "https://www.dropbox.com/scl/fi/21h43ppd999i9b4wszi5m/12.mp3?rlkey=fbqh8hi31bow5cp7wklv19v3y&st=2b8zmcag&dl=1",
+    },
+
+    {
+      title: "غضب جلسه 1",
+      url: "https://www.dropbox.com/scl/fi/virpbx20rjt5galhkxbzh/13.mp3?rlkey=fqto073o5pbchltaotfr109zp&st=grbrmvn2&dl=1",
+    },
+
+    {
+      title: "غضب جلسه 2",
+      url: "https://www.dropbox.com/scl/fi/r7hla81d9ltm94pcl815k/14.mp3?rlkey=g6tlyb8q53o5p5ac1vp427j3t&st=vdy5ln68&dl=1",
+    },
+
+    {
+      title: "غیبت جلسه 1",
+      url: "https://www.dropbox.com/scl/fi/1jh14grkq5c7jltgyhowj/15.mp3?rlkey=vbjgogsupv7o0ocfsa8kzlkza&st=ci50opaa&dl=1",
+    },
+
+    {
+      title: "غیبت جلسه 2",
+      url: "https://www.dropbox.com/scl/fi/qsdrpkcyefcp9f93u2k9y/16.mp3?rlkey=vy40syda3ofku4m5gp5gabddc&st=cbfdk3lw&dl=1",
+    },
+
+    {
+      title: "نفاق جلسه 1",
+      url: "https://www.dropbox.com/scl/fi/5i2iqkg0ugkhi7s27pdq4/17.mp3?rlkey=ulri5t4m7zxvwhl00v68438s5&st=kd1am8u9&dl=1",
+    },
+
+    {
+      title: "نفاق جلسه 2",
+      url: "https://www.dropbox.com/scl/fi/amu5z7k7utoc4f6hrxf7l/18.mp3?rlkey=7e93zo69lbrb2cv8k5f6fxrs9&st=l73iqbw6&dl=1",
+    },
+
+    {
+      title: "تعصب",
+      url: "https://www.dropbox.com/scl/fi/62qh6g05uo6vconz63ykn/19.mp3?rlkey=f1vmlqc9cw1faglxbdy6tlunw&st=urtpmqsw&dl=1",
+    },
+
+    {
+      title: "حسد جلسه 1",
+      url: "https://www.dropbox.com/scl/fi/35ylh3ed4fwomph4zrsk8/20.mp3?rlkey=17rewvu79egyzee0ivm1sxjr2&st=xlkf99o2&dl=1",
+    },
+
+    {
+      title: "حسد جلسه 2",
+      url: "https://www.dropbox.com/scl/fi/qm8kzmdd7w5ug11ha8uyk/21.mp3?rlkey=jnw7o23z0lswbxyqey1a8hnpp&st=k5iszdsz&dl=1",
+    },
+
+    {
+      title: "عجب جلسه 1",
+      url: "https://www.dropbox.com/scl/fi/v8r4f6yi7cxuk5ljl31tj/22.mp3?rlkey=09gvkjutypjlcrcxxa56rwmk0&st=j2ai9xm3&dl=1",
+    },
+
+    {
+      title: "عجب جلسه 2",
+      url: "https://www.dropbox.com/scl/fi/z0l7o3sc93ekywx2iggpp/23.mp3?rlkey=nrbkk1ubn0axnb2klosxjfps8&st=yngzxvf2&dl=1",
+    },
+
+    {
+      title: "ریا جلسه 1",
+      url: "https://www.dropbox.com/scl/fi/akfbj37610nflcj6rovfk/24.mp3?rlkey=aaxnaa5ydy8cbfzhwzoeqck5i&st=mim34u8e&dl=1",
+    },
+
+    {
+      title: "ریا جلسه 2",
+      url: "https://www.dropbox.com/scl/fi/2zl88n8j6jik4niybc5dp/25.mp3?rlkey=dnhh8fzzue1axid8rn74al71p&st=f18l57r5&dl=1",
+    },
+
+    {
+      title: "ریا جلسه 3",
+      url: "https://www.dropbox.com/scl/fi/yh84c2odpw2xf8eov5cbp/26.mp3?rlkey=eq51zazeg8iddzv7eemt7k3ve&st=1halkxrb&dl=1",
+    },
+
+    {
+      title: "دعا",
+      url: "https://www.dropbox.com/scl/fi/auck20y8ewe2jjkuh0aay/27.mp3?rlkey=5gy6t06mhjgce4b1mvjqavbr5&st=wuuabutq&dl=1",
+    },
+  ];
