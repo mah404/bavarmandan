3a79182c5b118c300f2667a1d8f31de7b40d8558
mahtabvariyani <mahtabvariyani@gmail.com>
2026-03-24 19:38:07 +0100

    Added the news

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import { BenefitsSection } from "@/components/layout/sections/benefits";
 import { CommunitySection } from "@/components/layout/sections/community";
-import { FAQSection } from "@/components/layout/sections/faq";
-import { FeaturesSection } from "@/components/layout/sections/features";
+
 import { FooterSection } from "@/components/layout/sections/footer";
 import { HeroSection } from "@/components/layout/sections/hero";
 import { ServicesSection } from "@/components/layout/sections/services";
-import { SponsorsSection } from "@/components/layout/sections/sponsors";
-import { TeamSection } from "@/components/layout/sections/team";
-import { TestimonialSection } from "@/components/layout/sections/testimonial";
+
 
 export const metadata = {
   title: "مجمع باورمندان کلاب هاوس",
