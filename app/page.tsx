1087b39f58f38bdd2ef1007e7f55c0f0b0fddbb9
mahtabvariyani <mahtabvariyani@gmail.com>
2026-03-24 19:34:53 +0100

    Added the news

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { FAQSection } from "@/components/layout/sections/faq";
 import { FeaturesSection } from "@/components/layout/sections/features";
 import { FooterSection } from "@/components/layout/sections/footer";
 import { HeroSection } from "@/components/layout/sections/hero";
-import { PricingSection } from "@/components/layout/sections/pricing";
 import { ServicesSection } from "@/components/layout/sections/services";
 import { SponsorsSection } from "@/components/layout/sections/sponsors";
 import { TeamSection } from "@/components/layout/sections/team";
